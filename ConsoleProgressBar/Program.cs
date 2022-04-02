6907d7797610aab61f74fb1dc6029064c784cb93
github-lym <allregister@gmail.com>
2022-04-02 22:43:40 +0800

    Init Commit

---
+++ ConsoleProgressBar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleProgressBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Performing some task... ");
+            using (var progress = new ConsoleProgressBar())
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    progress.Report((double)i / 100);
+                    Thread.Sleep(20);
+                }
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+忘記從哪裡看來的，就感覺美觀實用而留了下來。  
+  
+![](ConsoleProgressBar.gif)
